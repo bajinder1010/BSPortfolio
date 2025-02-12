87039034c55ceef23797336185badc5b71cbf5cc
Bajinder Singh <bajindersingh@192.168.1.157>
2025-02-12 20:31:31 +0000

    added link

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,7 +62,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-extrabold tracking-wide  text-5xl text-[#f6f9fc]">Hello, I&apos;m Bajinder Singh</span> 
-        <div className="text-3xl py-1  leading-snug text-[#8ea9bf]">Senior Software Engineer currently based in London,UK, and employed at TCS. My professional journey includes collaborating with clients worldwide, contributing to a global perspective in software development.
+        <div className="text-3xl py-1  leading-snug text-[#8ea9bf]">Senior Software Engineer currently based in London,UK, and employed at <a href="https://www.tcs.com/" target="_blank" className="font-medium text-cyan-300 underline dark:text-blue-100 hover:no-underline">tcs</a>. My professional journey includes collaborating with clients worldwide, contributing to a global perspective in software development.
 
         I develop things in my free time.</div>
       </motion.h1>
