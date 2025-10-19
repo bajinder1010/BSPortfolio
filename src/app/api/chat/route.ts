1cf559a6442a65803aa9d4dbc8076ea2831bb0c2
Bajinder Singh <bajindersingh@192.168.1.109>
2025-10-19 19:29:01 +0100

    try1

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,13 +6,12 @@ import OpenAI from 'openai';
 import cosineSimilarity from 'cosine-similarity';
 
 if (!admin.apps.length) {
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+  );
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey,
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
