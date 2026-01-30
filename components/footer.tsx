2f0c9a9d486ab7635b03c492a93c198ec2f3af1c
Bajinder Singh <bajindersingh@192.168.1.173>
2026-01-30 19:31:07 +0000

    yesr change

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,11 @@ export default function Footer() {
   return (
     <footer className="mb-8  mt-8 px-4 text-center text-[#666666]">
       <small className="mb-2 block text-xs">
-        &copy; 2025 Baj-inder.com All rights reserved.
+        &copy; 2026 Baj-inder.com All rights reserved.
       </small>
       <p className="text-xs">
-        Built using Next.js, TypeScript, Tailwind CSS,
-        Vercel hosting.
+        Built using Next.js, TypeScript, Tailwind CSS, Vercel hosting.
       </p>
-      
     </footer>
   );
 }
